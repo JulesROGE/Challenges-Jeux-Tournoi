2f65094df662ec777d9713f37f393ad40ed173fe
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2023-10-28 13:42:42 +0200

    Suppression fichiers inutiles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 // Autor: Jules ROGÉ
-import UserCard from "./components/UserCard";
-import  Timeline  from "./components/Timeline";
-import UserCardTest from "./components/UserCardTest";
 import Roue from "./components/Roue";
 
 import "./App.sass";
@@ -9,21 +6,6 @@ import "./App.sass";
 function App() {
   return (
     <div className="App" id="IDP1">
-      <h1 className="titre">Cards</h1>
-
-      <UserCard />
-
-      {/* <h1 className="titre">Old Cards</h1> */}
-
-      {/* <UserCardOld /> */}
-
-      {/* <h1 className="titre">Test Cards</h1> */}
-
-      {/* <UserCardTest /> */}
-
-      <h1 className="titre">Timeline</h1>
-      
-      <Timeline />
 
       <h1 className="titre">Roue</h1>
 
