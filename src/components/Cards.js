796b7bc56eb0645704ef91e5f292a64dd26fcb9e
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2023-11-05 12:00:58 +0100

    Déplacement style d'un fichier à un autre plus ajout div pour la gestion des cards.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -36,80 +36,79 @@ export default function ActionAreaCard() {
 
   return (
     <div className="Cards">
-      <div className="Card">
-        <Card sx={{ maxWidth: 345 }} className="Card">
-          <CardActionArea>
-            <CardMedia
-              onClick={handleOpenValorant}
-              component="img"
-              height="140"
-              image="https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-b88adde6a57e40aa85818820aa87a6cd"
-              alt="green iguana"
-            />
-
-          </CardActionArea>
-        </Card>
-        <Modal
-          open={openValorant}
-          onClose={handleCloseValorant}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <h1 className="titre">Challenge Valorant</h1>
-            <Valorant />
-          </Box>
-        </Modal>
-      </div>
-      <div className="Card">
-        <Card sx={{ maxWidth: 345 }} className="Card">
-          <CardActionArea>
-            <CardMedia
-              onClick={handleOpenRocketLeague}
-              component="img"
-              height="140"
-              image="https://cdn1.epicgames.com/offer/9773aa1aa54f4f7b80e44bef04986cea/EGS_RocketLeague_PsyonixLLC_S1_2560x1440-0f2f0dbbb161b884d50f2ca09f4110bf"
-              alt="green iguana"
-            />
-
-          </CardActionArea>
-        </Card>
-        <Modal
-          open={openRocketLeague}
-          onClose={handleCloseRocketLeague}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <h1 className="titre">Challenge Rocket League</h1>
-            <h2 className="titre">PlayAbility</h2>
-          </Box>
-        </Modal>
-      </div>
-      <div className="Card">
-        <Card sx={{ maxWidth: 345 }} className="Card">
-          <CardActionArea>
-            <CardMedia
-              onClick={handleOpenFortnite}
-              component="img"
-              height="140"
-              image="https://cdn.sortiraparis.com/images/80/66131/908390-fortnite-enfer-vert-map-skins-passe-de-combat-le-point-sur-les-nouveautes-de-la-saison-3.jpg"
-              alt="green iguana"
-            />
-
-          </CardActionArea>
-        </Card>
-        <Modal
-          open={openFortnite}
-          onClose={handleCloseFortnite}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <h1 className="titre">Challenge Fortnite</h1>
-            <Fortnite />
-          </Box>
-        </Modal>
+      <div className="CardsRow1">
+        <div className="Card">
+          <Card sx={{ maxWidth: 345 }} className="Card">
+            <CardActionArea>
+              <CardMedia
+                onClick={handleOpenValorant}
+                component="img"
+                height="140"
+                image="https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-b88adde6a57e40aa85818820aa87a6cd"
+                alt="green iguana"
+              />
+            </CardActionArea>
+          </Card>
+          <Modal
+            open={openValorant}
+            onClose={handleCloseValorant}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <h1 className="titre">Challenge Valorant</h1>
+              <Valorant />
+            </Box>
+          </Modal>
+        </div>
+        <div className="Card">
+          <Card sx={{ maxWidth: 345 }} className="Card">
+            <CardActionArea>
+              <CardMedia
+                onClick={handleOpenRocketLeague}
+                component="img"
+                height="140"
+                image="https://cdn1.epicgames.com/offer/9773aa1aa54f4f7b80e44bef04986cea/EGS_RocketLeague_PsyonixLLC_S1_2560x1440-0f2f0dbbb161b884d50f2ca09f4110bf"
+                alt="green iguana"
+              />
+            </CardActionArea>
+          </Card>
+          <Modal
+            open={openRocketLeague}
+            onClose={handleCloseRocketLeague}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <h1 className="titre">Challenge Rocket League</h1>
+              <h2 className="titre">PlayAbility</h2>
+            </Box>
+          </Modal>
+        </div>
+        <div className="Card">
+          <Card sx={{ maxWidth: 345 }} className="Card">
+            <CardActionArea>
+              <CardMedia
+                onClick={handleOpenFortnite}
+                component="img"
+                height="140"
+                image="https://cdn.sortiraparis.com/images/80/66131/908390-fortnite-enfer-vert-map-skins-passe-de-combat-le-point-sur-les-nouveautes-de-la-saison-3.jpg"
+                alt="green iguana"
+              />
+            </CardActionArea>
+          </Card>
+          <Modal
+            open={openFortnite}
+            onClose={handleCloseFortnite}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <h1 className="titre">Challenge Fortnite</h1>
+              <Fortnite />
+            </Box>
+          </Modal>
+        </div>
       </div>
     </div>
   );
